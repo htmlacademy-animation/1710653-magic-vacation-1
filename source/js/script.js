3e2026110ffed6cb3da4e76a23c8b9a30a045422
Дмитрий Князьков <knyazkovd@gmail.com>
2021-01-18 13:41:09 +0300

    [module-2-3] done subtask 5

--- source/js/script.js
+++ source/js/script.js
@@ -9,7 +9,7 @@ import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
 import pageLoadingSteps from './modules/page-loading-steps';
-import mainScreen from "./modules/mainScreen";
+import screens from "./modules/screens";
 
 // init modules
 pageLoadingSteps();
@@ -21,7 +21,7 @@ chat();
 result();
 form();
 social();
-mainScreen();
+screens();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
